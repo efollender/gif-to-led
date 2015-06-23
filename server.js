e8d18e6b78032720a7bac36f0c9d96acd6583566
Emma Follender <follender@Brooklyns-MacBook-Air.local>
2015-06-23 15:16:55 -0400

    Messily added led program control

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 //Lets require/import the HTTP module
 var http = require('http');
+var ws281x = require('rpi-ws281x-native'),
+    canvas = require('../index.js').create(32,32),
+    ctx = canvas.getContext('2d');
 
 //Lets define a port we want to listen to
 const PORT=8080; 
@@ -17,3 +20,16 @@ server.listen(PORT, function(){
     //Callback triggered when server is successfully listening. Hurray!
     console.log("Server listening on: http://localhost:%s", PORT);
 });
+
+function rnd(max) { return (max || 1) * Math.random(); }
+function rndi(max) { return Math.round(rnd(max)); }
+
+ws281x.init(100);
+
+setInterval(function() {
+    ctx.globalAlpha = 1;
+    ctx.fillStyle = '#' + rndi(0xffffff).toString(16);
+    ctx.fillRect(rndi(10)-2, rndi(10)-2, rndi(10), rndi(10));
+
+    ws281x.render(canvas.toUint32Array());
+}, 100);
